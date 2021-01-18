52a81b5f113271d4400f81223d05f26510211b4c
yehudis raitman <yehudisr@gmail.com>
2021-01-18 15:59:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,14 +26,15 @@ def monopoly_with_third_tier
   # complexity.
   {:railroads => {
      :pieces => 4, 
-     :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}, 
+     :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, 
+     :three_pieces_owned => 100, 
+     :four_pieces_owned => 200}},
      :names => {:reading_railroad => {}, 
      :pennsylvania_railroad => {},
      :b_and_o_railroad => {},
      :shortline_railroad => {}
    }
    }
-
 end
 
 def monopoly_with_fourth_tier
